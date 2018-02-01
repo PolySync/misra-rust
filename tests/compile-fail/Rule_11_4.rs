18a572720f1aa3ca33f6cfca64d3f41e4dec68bd
Shea Newton <snewton@polysync.io>
2018-01-31 16:16:33 -0800

    wrong error reported in compile tests for rule 11.4

--- tests/compile-fail/Rule_11_4.rs
+++ tests/compile-fail/Rule_11_4.rs
@@ -10,7 +10,7 @@ struct TypeA {
 }
 
 fn main() {
-    let i16a: i16 = 1;
-    let u32a: u32 = &i16a as &u16; //~ ERROR mismatched types
+    let i16_a: i16 = 1;
+    let u32_b: u32 = &i16_a as &u16; //~ ERROR mismatched types
     //~^ ERROR non-primitive cast: `&i16` as `&u16`
 }
